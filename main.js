b6340f157178fdbefefbcc4de84f0e036b2c8f92
Paramecio0 <alicesoro@outlook.com>
2024-01-31 17:15:00 +0800

    checkCol & drawObj

--- main.js
+++ main.js
@@ -2,30 +2,26 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContsxt("2d");
 
-//keydown
-document.addEventListener("keydown", obj_falling.rotate(key));
-
 //type(angle=0)
 //1 ****    2 **    3 **   4 **
 //            **      *       *
 //                    *       *
+//
 //5 **      6 **    7  *
 //   **      **       ***
+//
 
 //class
-class obj_falling
+var data=[[], [], [], []];
+var obj_falling = function()
 {
-    constructor()
-    {
-        this.type=Math.floor(Math.random()*7)+1;
-        this.shape=Shape[this.type];
-        this.angle=0;
-        //this.color=Color[this.type];
-        this.x=40;
-        this.y=0;
-    }
+    this.x=3;
+    this.y=0;
+    this.type=Math.floor(Math.random()*7)+1;
+    this.shape=Shape[this.type];
+    //this.color=Color[this.type];
 
-    move(key)
+    this.move = function(key)
     {
         switch(key.keyCode)
         {
@@ -36,27 +32,73 @@ class obj_falling
         }
     }
 
-    fall()
+    this.fall = function()
     {
         y+=10;
     }
 
-    rotate(key)
+    this.rotate = function(key)
     {
         if(key.keyCode == 38)
         {
-            angle+=90;//clockwise
-            if(angle == 360) {angle=0;}
+            var tmp=[[], [], [], []];
+            for(let i=0; i<4; i++)
+            {
+                tmp[i][0]=data[i][1];
+                tmp[i][1]=-data[i][0];
+            }
+            if(checkCol(tmp) == 1)
+            {for(let i=0; i<4; i++)
+                {
+                    data[i][0]=tmp[i][0];
+                    data[i][1]=tmp[i][1];
+                }
+            }
         }
     }
 
-    draw()
+    this.draw = function()
     {
-        for(i=0;i<4;i++)
+        location=translate(x,y,data);
+        for(i=0;i++;i<4)
         {
-
+            drawSquare(data[i][0]*10,data[i][1]*10);
         }
     }
+}
+
+//keydown
+document.addEventListener("keydown", obj_falling.rotate(key));
+
+//collision detect
+function checkCol(x,y,orig)
+{   
+    next=translate(x,y,orig);
+    var flag=1,i=0;
+    do{
+        var sx=next[i][0],sy=next[i][1];
+        if(board[sx][sy]==1)
+        {
+            flag=0;
+        }
+        i++;
+
+    }while(flag==1 && i<4)
+    return flag
+}
+
+//translate
+function translate(x,y,orig)//2-dimensional directly use(?)
+{
+    var tmp=[];
+    
+    for(let i=0; i<4; i++)
+        {
+            tmp[i][0]=orig[i][0];
+            tmp[i][1]=orig[i][1];
+        }
+
+    return tmp;
 }
 
 //2-d board
@@ -68,6 +110,33 @@ for (var i = 0; i < row; i++) {
   for (var j = 0; j < col; j++) {
     board[i][j] = 0;
   }
+}
+
+//clear line
+function clearline(i) {
+    for (let j = 0; j < 10; j++) {
+        board[i][j] = 0;
+    }
+    for (let j = i; j > 0; j--) {
+        for (let k = 0; k < 10; k++) {
+            board[j][k] = board[j - 1][k];
+        }
+    }
+}
+
+function checkline() {
+    for (let i = 0; i < 20; i++) {
+        let fullLine = true;
+        for (let j = 0; j < 10; j++) {
+            if (board[i][j] == 0) {
+                fullLine = false;
+                break;
+            }
+        }
+        if (fullLine) {
+            clearline(i);
+        }
+    }
 }
 
 //draw 小函数
@@ -111,14 +180,7 @@ function drawAll(){
     drawMap();
     drawBoard();
     //draw (falling) object
-    draw();
-}
-
-
-function ifcolli(){
-    if(){
-
-    }
+    obj_falling. draw();
 }
 
 function iffail(){
