262c7f24b28ac3d100c8a5ff5ffb7d46c3c86554
aspartame777 <158162651+aspartame777@users.noreply.github.com>
2024-02-01 20:30:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 //100*200
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const delay = 5;
-var photogram = 0;
+var score = document.getElementById('score');
 var TIMER = document.getElementById('timer');
 var time=0;
+const delay = 5;
+var photogram = 0;
+var sco=0;
+var bonus=-1;
 
 //type(angle=0)
 //1 ****    2 **    3 **   4 **
@@ -18,12 +21,8 @@ var time=0;
 var row = 20;
 var col = 10;
 var board = [];
-for (var i = 0; i < col; i++) {
-    board[i] = [];
-    for (var j = 0; j < row; j++) {
-        board[i][j] = 0;
-    }
-}
+var board_color= [];
+//board initialization is moved into function"initialize" to reset the game
 
 //shape
 var Shape =
@@ -37,6 +36,19 @@ var Shape =
         [[-1, -1], [0, -1], [0, 0], [1, 0]],
         [[-1, 0], [0, 0], [0, 1], [1, 0]]
     ];
+
+//color
+var Color =
+    [
+        "#000000",
+        "#FFADAD",
+        "#FFD6A5",
+        "#FDFFB6",
+        "#CAFFBF",
+        "#9BF6FF",
+        "#A0C4FF",
+        "#FFC6FF"
+    ]
 
 //class
 var data = [[], [], [], []];
@@ -54,7 +66,7 @@ var obj_falling = function () {
     this.draw = function () {
         let location = translate(this.x, this.y, data);
         for (let i = 0; i < 4; i++) {
-            drawSquare(location[i][0] * 10, location[i][1] * 10);
+            drawSquare(location[i][0] * 30, location[i][1] * 30, this.type);
         }
     }
 }
@@ -79,6 +91,10 @@ function checkCol(block,local) {
         if (board[sx][sy] == 1) {
             flag = 0;
         }
+        else if(sx > col || sx < 0)
+        {
+            flag = 0;
+        }
         i++;
     } while (flag == 1 && i < 4)
     return flag;
@@ -87,15 +103,16 @@ function checkCol(block,local) {
 
 //clear line
 function clearline(i) {
-    for (let j = 0; j < 20; j++) {
-        board[i][j] = 0;
+    for (let j = 0; j < 10; j++) {
+        board[j][i] = 0;
     }
     for (let j = i; j > 0; j--) {
-        for (let k = 0; k < 20; k++) {
-            board[j][k] = board[j - 1][k];
+        for (let k = 0; k < 10; k++) {
+            board[k][j] = board[k][j-1];
         }
     }
 }
+
 function checkline() {
     for (let i = 0; i < 20; i++) {
         let fullLine = true;
@@ -107,28 +124,31 @@ function checkline() {
         }
         if (fullLine) {
             clearline(i);
+            bonus++;
         }
     }
 }
 
 //draw 小函数
 //单个格子
-function drawSquare(x, y) {
+function drawSquare(x, y, colorType) {
     ctx.beginPath();
-    ctx.fillRect(x + 1, y + 1, 8, 8);
+    ctx.fillStyle = Color[colorType];
+    ctx.fillRect(x + 2, y + 2, 26, 26);
     ctx.stroke();
 }
+
 function drawMap() {
     ctx.beginPath();
-    for (let i = 0; i <= 100; i += 10) {
+    for (let i = 0; i <= 300; i += 30) {
         ctx.moveTo(i, 0);
-        ctx.lineTo(i, 200);
+        ctx.lineTo(i, 600);
         ctx.stroke();
     }
 
-    for (let i = 0; i <= 200; i += 10) {
+    for (let i = 0; i <= 600; i += 30) {
         ctx.moveTo(0, i);
-        ctx.lineTo(100, i);
+        ctx.lineTo(300, i);
         ctx.stroke();
     }
 }
@@ -137,14 +157,14 @@ function drawBoard() {
     for (var i = 0; i < col; i++) {
         for (var j = 0; j < row; j++) {
             if (board[i][j] != 0) {
-                drawSquare(i * 10, j * 10);
+                drawSquare(i * 30, j * 30, board_color[i][j]);
             }
         }
     }
 }
 
 function drawAll(block) {
-    ctx.clearRect(0, 0, 100, 200); // Clear the canvas
+    ctx.clearRect(0, 0, 300, 600); // Clear the canvas
     drawMap();
     drawBoard();
     //draw (falling) object
@@ -163,7 +183,9 @@ function resetblock(block) {
     if (colFlag) {
         for (let i = 0; i < 4; i++) {
             board[data[i][0] + block.x][data[i][1] + block.y] = 1;
+            board_color[data[i][0] + block.x][data[i][1] + block.y] = block.type;
         }
+
         // delete block;
         block.x = 4;
         block.y = -1;
@@ -172,10 +194,44 @@ function resetblock(block) {
     }
 }
 
-function initialize() {
+function fail()
+{
+    for(let i=0; i<col; i++)
+    {
+        if(board[i][0] == 1)
+        {
+            alert("GAME OVER");
+            restart();
+        }
+    }
+}
+
+function restart()
+{
+    block = new obj_falling();
+    time = 0;
+    sco = 0;
+
+    for (let i = 0; i < col; i++) {
+        board[i] = [];
+        for (let j = 0; j < row; j++) {
+            board[i][j] = 0;
+        }
+    }
+}
+
+function initialize()
+{
     //create a block
     block = new obj_falling();
-    console.log(block);
+
+    for (let i = 0; i < col; i++) {
+        board[i] = [];
+        board_color[i] = [];
+        for (let j = 0; j < row; j++) {
+            board[i][j] = 0;
+        }
+    }
 
     //listen to the keyboard
     //rotate
@@ -202,12 +258,32 @@ function initialize() {
         switch (key.keyCode)
         {
             case 37:
-                if (block.x > 0) { block.x -= 1; } break;
+                var tmp = [[], [], [], []];
+                for (let i = 0; i < 4; i++) {
+                    tmp[i][0] = data[i][0] - 1;
+                    tmp[i][1] = data[i][1];
+                }
+                
+                if (checkCol(block,tmp) == 1) { block.x -= 1; } break;
             case 39:
-                if (block.x < 9) { block.x += 1; } break;
+                var tmp = [[], [], [], []];
+                for (let i = 0; i < 4; i++) {
+                    tmp[i][0] = data[i][0] + 1;
+                    tmp[i][1] = data[i][1];
+                }
+
+                if (checkCol(block,tmp) == 1) { block.x += 1; } break;
         }
-    }
-);
+    });
+
+    //speed up
+    document.addEventListener("keydown", function(key){
+        if (key.keyCode == 40)
+        {
+            photogram = delay;
+        }
+    })
+
 
     drawAll(block);
 
@@ -217,14 +293,33 @@ function initialize() {
 
 function main(block) {
     drawAll(block);
-    resetblock(block);
+    fail();
+    checkline();
+
+    if(bonus!=-1){
+        sco=sco+10*Math.pow(2,bonus);
+        bonus=-1;
+    }
+    score.innerHTML=sco;
+    TIMER.innerHTML = time.toFixed(2);
+    time+=0.1;
+
     //if a block falls to the bottom, create a new one
+    resetblock(block);
+    
     block.fall();
     photogram++;
-    TIMER.innerHTML = time.toFixed(2);
-    time+=0.2;
 }
 
-//console.log(block);
-//console.log(object);
-//console.log(data);
+
+function askForNameAndGreet() {
+    var name = prompt("Please Enter Your Name:");
+
+    if (name === null || name.trim() === "") {
+    alert("Hello!");
+    } else {
+    var greetingMessage = document.getElementById('greetingMessage');
+    greetingMessage.textContent = "Hello! " + name + "! This is Tetris!";
+    }
+}
+askForNameAndGreet();
